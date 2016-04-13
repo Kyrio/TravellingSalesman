44d90c1d458f5ca96292d636210e5a62df1fb64a
Kyrio <pou003@gmail.com>
2016-04-13 11:33:50 +0200

    Toutes les heuristiques sauf Kruskal.

--- TSP.py
+++ TSP.py
@@ -2,37 +2,55 @@ from node import Node, RoadMap
 import math
 
 
-def build_node(estim_g, estim_h, liste_som, next_nodes):
+def build_node(road_map, old_node, new_city, next_nodes):
+    movement_cost = road_map.edges[old_node.current_city][new_city]
+    liste_som = old_node.liste_som + [new_city]
+    estim_g = old_node.estim_g + movement_cost
+    estim_h = h_local_min(road_map, new_city)
     return Node(estim_g, estim_h, liste_som, next_nodes)
 
 
-def compute_h():
+def h_zero():
     return 0
 
 
-def develop_node(node, r_map):
+def h_kruskal():
+    return 0
+
+
+def h_local_min(road_map, current_city):
+    distances = road_map.edges[current_city]
+    return min(distances)
+
+
+def h_edges_min(road_map, node):
+    visited = node.liste_som
+    sum_distance = 0
+    cities = road_map.cities
+    for i in cities:
+        if i not in visited:
+            sum_distance += min(road_map.edges[i])
+    return sum_distance
+
+
+def develop_node(road_map, node):
     nodes = []
     visited = node.liste_som
-    total_cities = r_map.cities
+    total_cities = road_map.cities
     for i in total_cities:
         if i not in visited:
-            movement_cost = r_map.edges[node.current_city][i]
-
-            new_liste_som = node.liste_som + [i]
-            new_estim_g = node.estim_g + movement_cost
-            new_estim_h = compute_h()
-
-            new_node = build_node(new_estim_g, new_estim_h, new_liste_som, [])
-
+            new_node = build_node(road_map, node, i, [])
             node.next_nodes.append(new_node)
             nodes.append(new_node)
+        # TODO création des feuilles -> if len(new_node.liste_som) == len(total_cities):
+            # TODO  build_node(....) (build feuille)
 
     return nodes
 
 
 def grp_size(nb_cities):
     size = 0
-    for i in range(1, nb_cities+1):
-        size += math.factorial(nb_cities-1)/math.factorial(nb_cities-i)
+    for i in range(1, nb_cities + 1):
+        size += math.factorial(nb_cities - 1) / math.factorial(nb_cities - i)
     size += math.factorial(nb_cities - 1)
     return size
--- node.py
+++ node.py
@@ -6,7 +6,7 @@ class Node:
         self.estim_g = estim_g
         self.estim_h = estim_h
         self.liste_som = liste_som  # liste des villes
-        self.next_nodes = next_nodes  # suite de la liste (successeur du noeud)
+        self.next_nodes = next_nodes  # suite de la liste (successeurs du noeud)
 
     @property
     def estim_f(self):
@@ -33,7 +33,7 @@ class RoadMap:
     def make_edges(self):
         edges = [[0 for x in range(self.nb_cities)] for y in range(self.nb_cities)]
         for i in range(self.nb_cities):
-            edges[i][i] = -1
+            edges[i][i] = float('inf')
             for j in range(self.nb_cities):
                 if i != j:
                     edges[i][j] = random.randint(1, 100)
